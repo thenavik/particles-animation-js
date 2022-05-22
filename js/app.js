c6ffc59ecf4c4b598d4e948993ae4cf42e970256
thenavik <iamdigitalist@gmail.com>
2022-05-22 17:03:56 +0300

    particles animated

---
+++ js/app.js
@@ -0,0 +1,96 @@
+(() => {
+  const cnv = document.querySelector("canvas");
+  const ctx = cnv.getContext("2d");
+
+  const cfg = {
+    orbsCount: 400,
+    minVelocity: 0.2,
+    ringsCount: 10,
+  };
+
+  let mx = 0,
+    my = 0;
+  cnv.addEventListener(`mousemove`, (e) => {
+    mx = e.clientX - cnv.getBoundingClientRect().left;
+    my = e.clientY - cnv.getBoundingClientRect().top;
+  });
+
+  let cw, ch, cx, cy, ph, pw;
+  function resize() {
+    cw = cnv.width = innerWidth;
+    ch = cnv.height = innerHeight;
+    cx = cw * 0.5;
+    cy = ch * 0.5;
+    ph = cy * 0.4;
+    pw = cx * 0.4;
+  }
+  resize();
+  window.addEventListener("resize", resize);
+
+  class Orb {
+    constructor() {
+      this.size = Math.random() * 5 + 2;
+      this.angle = Math.random() * 360;
+      this.radius =
+        (((Math.random() * cfg.ringsCount) | 0) * ph) / cfg.ringsCount;
+      this.impact = this.radius / ph;
+      this.velocity = cfg.minVelocity + Math.random() * cfg.minVelocity;
+    }
+
+    refresh() {
+      let radian = (this.angle * Math.PI) / 180;
+
+      let cos = Math.cos(radian);
+      let sin = Math.sin(radian);
+
+      let offsetX = cos * pw * this.impact;
+      let offsetY = sin * pw * this.impact;
+
+      let paralaxX = (mx / cw) * 2 - 1;
+      let paralaxY = my / ch;
+
+      let x = cx + cos * (ph + this.radius) + offsetX;
+      let y =
+        cy + sin * (ph + this.radius) - offsetY * paralaxY - paralaxX * offsetX;
+
+      let distanceToC = Math.hypot(x - cx, y - cy);
+
+      let optic = sin * this.size * this.impact * 0.7;
+
+      let size = this.size + optic;
+
+      // Color Palette
+      let shade = this.angle;
+      let saturation = 100;
+      let brightness = (1 - Math.sin(this.impact * Math.PI)) * 90 + 10;
+      let color = `hsl(${shade}, ${saturation}%, ${brightness}%)`;
+
+      if (distanceToC > ph - 1 || sin > 0) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, size, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+
+      this.angle = (this.angle + this.velocity) % 360;
+    }
+  }
+
+  let orbsList = [];
+  function createStardust() {
+    for (let i = 0; i < cfg.orbsCount; i++) {
+      orbsList.push(new Orb());
+    }
+  }
+  createStardust();
+
+  function loop() {
+    requestAnimationFrame(loop);
+    ctx.globalCompositeOperation = `normal`;
+    ctx.fillStyle = `rgb(22, 22, 22)`;
+    ctx.fillRect(0, 0, cw, ch);
+    ctx.globalCompositeOperation = ` lighter`;
+    orbsList.map((e) => e.refresh());
+  }
+  loop();
+})();
